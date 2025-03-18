79a5141fb658696e5acee32d623df1a4f54e4099
maiconsbotelho <maicon.sbotelho@hotmail.com>
2025-03-18 16:39:43 -0300

    Initial commit

---
+++ funcoes.js
@@ -0,0 +1,21 @@
+// FUNCTION DECLARATION OU FUNÇÃO NOMEADA
+function bomDia() {
+  console.log("Bom dia!");
+  return "Bom dia!";
+}
+
+// FUNCTION EXPRESSION OU FUNÇÃO ANÔNIMA
+const boaTarde = function () {
+  console.log("Boa tarde!");
+  return "Boa tardee!";
+};
+
+// ARROW FUNCTION
+const boaNoite = () => {
+  console.log("Boa noite!");
+  return "Boa noite!";
+};
+
+bomDia();
+boaTarde();
+boaNoite();
